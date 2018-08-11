61cc639edb0c49dce86b2684587a569e2aee5e46
Jtuzi <944313281@qq.com>
2018-08-11 10:48:11 +0800

    CrackMe

---
+++ CrackMe/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrackMe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Jtuzi")
+            {
+                MessageBox.Show("你成功了！膜拜大牛~");
+            }
+        }
+    }
+}
